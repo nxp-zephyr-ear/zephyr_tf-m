b156e52522b9aabd44fa5ed275065ca11610ad4f
Stefan Krug <stefan.krug@nxp.com>
2023-11-09 17:09:45 +0100

    [nxp noup] platform: rdrw61x: TF-M 1.8 specific updates from NXP SDK.
    Merge in files from SDK TF-M port for TF-M version 1.8+.
    
    NXP internal reference:
    Source: https://bitbucket.sw.nxp.com/projects/MCUCORE/repos/mcu-sdk-tfm/browse
    Revision: b5519b07 [WCBRD-1564] typo error fixed. [Waqar Ali Tahir]
    
    Signed-off-by: Stefan Krug <stefan.krug@nxp.com>
    Signed-off-by: Waqar Ali Tahir <waqar.tahir@nxp.com>

--- platform/ext/target/nxp/rdrw61x/crypto_keys.c
+++ platform/ext/target/nxp/rdrw61x/crypto_keys.c
@@ -15,19 +15,14 @@
  * limitations under the License.
  */
 
-#include "tfm_plat_crypto_keys.h"
-
-#include "tfm_builtin_key_ids.h"
-#include "tfm_builtin_key_loader.h"
-#include "psa_manifest/pid.h"
-#include "tfm_plat_otp.h"
-
-#include "mcuxClEls.h"
-
-#include <stddef.h>
 #include <string.h>
-#include <stdint.h>
+#include "tfm_plat_crypto_keys.h"
+#include "tfm_builtin_key_ids.h"
+#include "tfm_plat_otp.h"
+#include "psa_manifest/pid.h"
+#include "tfm_builtin_key_loader.h"
 
+#define NUMBER_OF_ELEMENTS_OF(x) sizeof(x)/sizeof(*x)
 #define TFM_NS_PARTITION_ID -1
 
 #ifndef MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER
@@ -35,92 +30,6 @@
 #endif
 
 #define PSA_KEY_LOCATION_S50_KEY_GEN_STORAGE ((psa_key_location_t)0xE00301)
-
-enum tfm_plat_err_t tfm_plat_builtin_key_get_usage(psa_key_id_t key_id,
-                                                   mbedtls_key_owner_id_t user,
-                                                   psa_key_usage_t *usage)
-{
-    *usage = 0;
-
-    switch (key_id) {
-    case TFM_BUILTIN_KEY_ID_HUK:
-        /* Allow access to all partitions */
-        *usage = PSA_KEY_USAGE_DERIVE;
-        break;
-	case TFM_BUILTIN_KEY_ID_EL2GO_CONN_AUTH:
-        *usage = PSA_KEY_USAGE_SIGN_HASH;
-	  	break;
-    case TFM_BUILTIN_KEY_ID_IAK:
-        switch(user) {
-#ifdef TFM_PARTITION_INITIAL_ATTESTATION
-        case TFM_SP_INITIAL_ATTESTATION:
-            *usage = PSA_KEY_USAGE_SIGN_HASH;
-#ifdef SYMMETRIC_INITIAL_ATTESTATION
-            /* Needed to calculate the instance ID */
-            *usage |= PSA_KEY_USAGE_EXPORT;
-#endif /* SYMMETRIC_INITIAL_ATTESTATION */
-            break;
-#if defined(TEST_S_ATTESTATION) || defined(TEST_NS_ATTESTATION)
-        /* So that the tests can validate created tokens */
-#ifdef TEST_S_ATTESTATION
-        case TFM_SP_SECURE_TEST_PARTITION:
-#endif /* TEST_S_ATTESTATION */
-#ifdef TEST_NS_ATTESTATION
-        case TFM_NS_PARTITION_ID:
-#endif /* TEST_NS_ATTESTATION */
-            *usage = PSA_KEY_USAGE_VERIFY_HASH;
-            break;
-#endif /* TEST_S_ATTESTATION || TEST_NS_ATTESTATION */
-#endif /* TFM_PARTITION_INITIAL_ATTESTATION */
-        default:
-            return TFM_PLAT_ERR_NOT_PERMITTED;
-        }
-        break;
-    default:
-        return TFM_PLAT_ERR_UNSUPPORTED;
-    }
-
-    return TFM_PLAT_ERR_SUCCESS;
-}
-
-enum tfm_plat_err_t tfm_plat_builtin_key_get_lifetime_and_slot(
-    mbedtls_svc_key_id_t key_id,
-    psa_key_lifetime_t *lifetime,
-    psa_drv_slot_number_t *slot_number)
-{
-    switch (MBEDTLS_SVC_KEY_ID_GET_KEY_ID(key_id)) {
-    case TFM_BUILTIN_KEY_ID_HUK:
-        *slot_number = TFM_BUILTIN_KEY_SLOT_HUK;
-        *lifetime = PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
-                        PSA_KEY_LIFETIME_PERSISTENT,
-                        TFM_BUILTIN_KEY_LOADER_KEY_LOCATION);
-        break;
-	case TFM_BUILTIN_KEY_ID_EL2GO_CONN_AUTH:
-        *slot_number = 0U;
-        *lifetime = PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
-                        PSA_KEY_LIFETIME_PERSISTENT,
-                        PSA_KEY_LOCATION_S50_KEY_GEN_STORAGE);
-		break;
-#ifdef TFM_PARTITION_INITIAL_ATTESTATION
-    case TFM_BUILTIN_KEY_ID_IAK:
-        *slot_number = TFM_BUILTIN_KEY_SLOT_IAK;
-#ifdef USE_ELS_PKC_IAK
-        *lifetime  = PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
-                        PSA_KEY_LIFETIME_PERSISTENT,
-                        PSA_KEY_LOCATION_S50_KEY_GEN_STORAGE);
-#else
-        *lifetime = PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
-                        PSA_KEY_LIFETIME_PERSISTENT,
-                        TFM_BUILTIN_KEY_LOADER_KEY_LOCATION);
-#endif
-#endif /* TFM_PARTITION_INITIAL_ATTESTATION */
-        break;
-    default:
-        return TFM_PLAT_ERR_UNSUPPORTED;
-    }
-
-    return TFM_PLAT_ERR_SUCCESS;
-}
 
 #if USE_ELS_PKC_HUK
 
@@ -162,7 +71,7 @@ enum tfm_plat_err_t tfm_plat_get_huk_els_pkc(uint8_t *buf, size_t buf_len, size_
     uint8_t key_buf[32]; /* 256-bit key. */
 
     // clang-format off
-    const uint8_t label_buf[32] = { 
+    const uint8_t label_buf[32] = {
           0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
           0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
           0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
@@ -262,12 +171,16 @@ exit:
 }
 
 #endif
+//ST end
 
-static enum tfm_plat_err_t tfm_plat_get_huk(
-    uint8_t *buf, size_t buf_len, size_t *key_len, size_t *key_bits, psa_algorithm_t *algorithm, psa_key_type_t *type)
+static enum tfm_plat_err_t tfm_plat_get_huk(uint8_t *buf, size_t buf_len,
+                                            size_t *key_len,
+                                            size_t *key_bits,
+                                            psa_algorithm_t *algorithm,
+                                            psa_key_type_t *type)
 {
     enum tfm_plat_err_t err;
-#if USE_ELS_PKC_HUK
+#if defined(PSA_CRYPTO_DRIVER_ELS_PKC) && USE_ELS_PKC_HUK
     err = tfm_plat_get_huk_els_pkc(buf, buf_len, key_len);
     if (err != TFM_PLAT_ERR_SUCCESS)
     {
@@ -338,55 +251,81 @@ static enum tfm_plat_err_t tfm_plat_get_iak(uint8_t *buf, size_t buf_len,
 }
 #endif /* TFM_PARTITION_INITIAL_ATTESTATION */
 
-enum tfm_plat_err_t tfm_plat_load_builtin_keys(void)
-{
-    psa_status_t err;
-    mbedtls_svc_key_id_t key_id = MBEDTLS_SVC_KEY_ID_INIT;
-    psa_key_attributes_t attr = PSA_KEY_ATTRIBUTES_INIT;
-    enum tfm_plat_err_t plat_err;
-    uint8_t buf[32];
-    size_t key_len;
-    size_t key_bits;
-    psa_algorithm_t algorithm;
-    psa_key_type_t type;
-
-    /* HUK */
-    plat_err = tfm_plat_get_huk(buf, sizeof(buf), &key_len, &key_bits,
-                                &algorithm, &type);
-    if (plat_err != TFM_PLAT_ERR_SUCCESS) {
-        return plat_err;
-    }
-    key_id.MBEDTLS_PRIVATE(key_id) = TFM_BUILTIN_KEY_ID_HUK;
-    psa_set_key_id(&attr, key_id);
-    psa_set_key_bits(&attr, key_bits);
-    psa_set_key_algorithm(&attr, algorithm);
-    psa_set_key_type(&attr, type);
-    err = tfm_builtin_key_loader_load_key(buf, key_len, &attr);
-    if (err != PSA_SUCCESS) {
-        return TFM_PLAT_ERR_SYSTEM_ERR;
-    }
-
 #ifdef TFM_PARTITION_INITIAL_ATTESTATION
-    /* IAK */
-#if defined(PSA_CRYPTO_DRIVER_ELS_PKC) && defined(USE_ELS_PKC_IAK)
-    // This a real built-in key, nothing to do.
+/**
+ * @brief Table describing per-user key policy for the IAK
+ *
+ */
+static const tfm_plat_builtin_key_per_user_policy_t g_iak_per_user_policy[] = {
+    {.user = TFM_SP_INITIAL_ATTESTATION,
+#ifdef SYMMETRIC_INITIAL_ATTESTATION
+        .usage = PSA_KEY_USAGE_SIGN_HASH | PSA_KEY_USAGE_EXPORT,
 #else
-    plat_err = tfm_plat_get_iak(buf, sizeof(buf), &key_len, &key_bits,
-                                &algorithm, &type);
-    if (plat_err != TFM_PLAT_ERR_SUCCESS) {
-        return TFM_PLAT_ERR_SYSTEM_ERR;
-    }
-    key_id.MBEDTLS_PRIVATE(key_id) = TFM_BUILTIN_KEY_ID_IAK;
-    psa_set_key_id(&attr, key_id);
-    psa_set_key_bits(&attr, key_bits);
-    psa_set_key_algorithm(&attr, algorithm);
-    psa_set_key_type(&attr, type);
-    err = tfm_builtin_key_loader_load_key(buf, key_len, &attr);
-    if (err != PSA_SUCCESS) {
-        return TFM_PLAT_ERR_SYSTEM_ERR;
-    }
-#endif
+        .usage = PSA_KEY_USAGE_SIGN_HASH,
+#endif /* SYMMETRIC_INITIAL_ATTESTATION */
+    },
+#ifdef TEST_S_ATTESTATION
+    {.user = TFM_SP_SECURE_TEST_PARTITION, .usage = PSA_KEY_USAGE_VERIFY_HASH},
+#endif /* TEST_S_ATTESTATION */
+#ifdef TEST_NS_ATTESTATION
+    {.user = TFM_NS_PARTITION_ID, .usage = PSA_KEY_USAGE_VERIFY_HASH},
+#endif /* TEST_NS_ATTESTATION */
+};
 #endif /* TFM_PARTITION_INITIAL_ATTESTATION */
 
-    return TFM_PLAT_ERR_SUCCESS;
+/**
+ * @brief Table describing per-key user policies
+ *
+ */
+static const tfm_plat_builtin_key_policy_t g_builtin_keys_policy[] = {
+    {.key_id = TFM_BUILTIN_KEY_ID_HUK, .per_user_policy = 0, .usage = PSA_KEY_USAGE_DERIVE},
+    {.key_id = TFM_BUILTIN_KEY_ID_EL2GO_CONN_AUTH, .per_user_policy = 0, .usage = PSA_KEY_USAGE_SIGN_HASH},
+#ifdef TFM_PARTITION_INITIAL_ATTESTATION
+    {.key_id = TFM_BUILTIN_KEY_ID_IAK,
+     .per_user_policy = NUMBER_OF_ELEMENTS_OF(g_iak_per_user_policy),
+     .policy_ptr = g_iak_per_user_policy},
+#endif /* TFM_PARTITION_INITIAL_ATTESTATION */
+};
+
+/**
+ * @brief Table describing the builtin-in keys (plaform keys) available in the platform. Note
+ *        that to bind the keys to the tfm_builtin_key_loader driver, the lifetime must be
+ *        explicitly set to the one associated to the driver, i.e. TFM_BUILTIN_KEY_LOADER_LIFETIME
+ */
+static const tfm_plat_builtin_key_descriptor_t g_builtin_keys_desc[] = {
+    {.key_id = TFM_BUILTIN_KEY_ID_HUK,
+     .slot_number = TFM_BUILTIN_KEY_SLOT_HUK,
+     .lifetime = TFM_BUILTIN_KEY_LOADER_LIFETIME,
+     .loader_key_func = tfm_plat_get_huk},
+     /* Would be nice to add EL2GO_CONN_AUTH change via pre-processor*/
+    {.key_id = TFM_BUILTIN_KEY_ID_EL2GO_CONN_AUTH,
+     .slot_number = 0U,
+     .lifetime = PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
+                        PSA_KEY_LIFETIME_PERSISTENT,
+                        PSA_KEY_LOCATION_S50_KEY_GEN_STORAGE),
+     .loader_key_func = NULL/*tfm_plat_get_el2go_auth*/},
+#ifdef TFM_PARTITION_INITIAL_ATTESTATION
+    {.key_id = TFM_BUILTIN_KEY_ID_IAK,
+     .slot_number = TFM_BUILTIN_KEY_SLOT_IAK,
+#if defined(PSA_CRYPTO_DRIVER_ELS_PKC) && USE_ELS_PKC_IAK
+     .lifetime = PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
+                        PSA_KEY_LIFETIME_PERSISTENT,
+                        PSA_KEY_LOCATION_S50_KEY_GEN_STORAGE),
+#else
+     .lifetime = TFM_BUILTIN_KEY_LOADER_LIFETIME,
+#endif /* USE_ELS_PKC_IAK */
+     .loader_key_func = tfm_plat_get_iak},
+#endif /* TFM_PARTITION_INITIAL_ATTESTATION */
+};
+
+size_t tfm_plat_builtin_key_get_policy_table_ptr(const tfm_plat_builtin_key_policy_t *desc_ptr[])
+{
+    *desc_ptr = &g_builtin_keys_policy[0];
+    return NUMBER_OF_ELEMENTS_OF(g_builtin_keys_policy);
+}
+
+size_t tfm_plat_builtin_key_get_desc_table_ptr(const tfm_plat_builtin_key_descriptor_t *desc_ptr[])
+{
+    *desc_ptr = &g_builtin_keys_desc[0];
+    return NUMBER_OF_ELEMENTS_OF(g_builtin_keys_desc);
 }
